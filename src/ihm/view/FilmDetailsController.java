9a460fdc1311b2e89a23222c3363f68b1d6eed51
MatthieuP <pauly.matthieu@gmail.com>
2017-03-03 22:29:32 +0100

    implement of listview + synopsis + imageview +title

--- src/ihm/view/FilmDetailsController.java
+++ src/ihm/view/FilmDetailsController.java
@@ -1,43 +1,79 @@
 package ihm.view;
 
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 
 import apiTheMovieDB.CineGoAPI;
+import apiTheMovieDB.CineGoFilm;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class FilmDetailsController {
 
-	
+
 	@FXML
-    private ListView<String> listView;
-	
+	private ListView<String> listView;
+
 	@FXML
-    private Label synopsis;
-	
+	private TextArea synopsis;
+
 	@FXML
-    private Label nomFilm;
-	
+	private Label nomFilm;
+
 	@FXML
-    private AnchorPane image;
-	
+	private ImageView viewImageFilm;
+
+	protected boolean APILoadOK = false;
+	protected List<CineGoFilm> cineGoFilms = new ArrayList<CineGoFilm>();
+	protected ObservableList<String> filmsList = FXCollections.observableArrayList();
+	//AJOUTER TAB IMAGE
+	//AJOUTER SYNOPSIS
+	//AJOUTER LES INFOS
+
 	public void initialize(){
-		
-    	List<String> listeIdFilms = new ArrayList<String>();			//Recuperer la liste de tout les films à l'affiche dans la base de donnee		
+		if (APILoadOK == false){
+			this.initAPI();
+			for( int i = 0 ; i < cineGoFilms.size() ; i++){
+				filmsList.add(cineGoFilms.get(i).getTitle());
+			}
+		}
+		listView.setItems(filmsList);
+		listView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				System.out.println("clicked on " + listView.getSelectionModel().getSelectedIndex());
+				nomFilm.setText(cineGoFilms.get(listView.getSelectionModel().getSelectedIndex()).getTitle());
+				synopsis.setText(cineGoFilms.get(listView.getSelectionModel().getSelectedIndex()).getOverview());
+				synopsis.setWrapText(true);
+				Image image = SwingFXUtils.toFXImage(cineGoFilms.get(listView.getSelectionModel().getSelectedIndex()).getDataIMG(), null);
+				viewImageFilm.setImage(image);
+			}
+		});
+	}
+
+	public void initAPI(){
+		List<String> listeIdFilms = new ArrayList<String>();		
+		//SIMULATION DE RECUPERATION DE LA LISTE DES ID DE FILM DANS LA BASE
 		listeIdFilms.add("121856");listeIdFilms.add("274870");listeIdFilms.add("47971");
 		CineGoAPI API = new CineGoAPI(listeIdFilms);
-		System.out.println(API.getTabFilms().toString());
-		
-		
-		ObservableList<String> wordsList = FXCollections.observableArrayList("First word","Second word", "Third word", "Etc.");
-    	listView.setItems(wordsList);
+		//System.out.println(API.getTabFilms().toString());
+		for( int i = 0 ; i < API.getTabFilms().size() ; i++){
+			cineGoFilms.add(API.getTabFilms().get(i));
+		}
+		APILoadOK = true;
 	}
-	
-	
+
 }
 
