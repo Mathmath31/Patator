0af03c6d45387a2cc48146a52998e225a4b90eb0
mathVM31 <le_math@live.fr>
2017-03-18 18:00:18 +0100

    création de salle choix de type de siege

--- src/ihm/view/ChoixPositionController.java
+++ src/ihm/view/ChoixPositionController.java
@@ -1,10 +1,7 @@
 package ihm.view;
 
-import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -17,8 +14,6 @@ public class ChoixPositionController {
 
 	private int nombreCol = 8;
 	private int nombreLig = 12;
-	private ReadOnlyDoubleProperty heightProperty;
-    private ReadOnlyDoubleProperty widthProperty;
     
 	@FXML
 	private Pane pane;
@@ -36,11 +31,8 @@ public class ChoixPositionController {
 		GridPane gridPane = new GridPane();
 		
 		gridPane.setGridLinesVisible(true);		
-
 		
-		
-		//int nombrelig = getRowCount(gridPane);
-		
+		// TODO Récupérer le nombre de ligne et colonne et les assigner a nombreCol et nombreLig
 		for (int i = 0; i < nombreCol ; i++) {
 			gridPane.getColumnConstraints().add(new ColumnConstraints(tailleCol / nombreCol));
 		}
@@ -50,10 +42,14 @@ public class ChoixPositionController {
         }
         pane.getChildren().add(0,gridPane);
         
+        
+        // TODO lire la salle appeler 
         for (int i = 0; i < nombreCol ; i++) {
 			for (int j = 0 ; j < nombreLig; j++) {
 				//ajout des rectangles
 				Rectangle rectangle = new Rectangle(tailleCol / (nombreCol) - 1, tailleLig / (nombreLig) - 1);
+				
+				// TODO remplacer le if par un select en fonction de la place
 				if(j == 2)
 				{
 					rectangle.setFill(Color.BLUE);
@@ -62,7 +58,10 @@ public class ChoixPositionController {
 				{
 					rectangle.setFill(Color.GREEN);	
 				}
+				
+				
 	            rectangle.setStroke(Color.WHITE);
+	            // evenement au click sur un rectangle
 	            rectangle.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
 	            	@Override
 	            	public void handle(MouseEvent e) {
@@ -74,7 +73,16 @@ public class ChoixPositionController {
 	        }
 		}    
 	}
-	
+	/**
+	 * Mise en forme au click
+	 * @author Math
+	 * @param i
+	 * @param j
+	 * @param rect
+	 * @return
+	 * @exception
+	 * @see
+	 */
 	public void click(int i, int j, Rectangle rect){
 		System.out.println(i + "/" + j);
 		rect.setFill(Color.BLACK);
--- src/ihm/view/CreationSalleController.java
+++ src/ihm/view/CreationSalleController.java
@@ -1,8 +1,11 @@
 package ihm.view;
 
 import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -26,6 +29,17 @@ public class CreationSalleController {
 	@FXML
 	private TextField nblig;
 	
+	@FXML
+	private Button normal;
+	
+	@FXML
+	private Button handi;
+	
+	@FXML
+	private Button couloir;
+	
+	@FXML 
+	private Button porte;
 	
 	public void initialize(){
 		pane.getChildren().clear();
@@ -39,11 +53,11 @@ public class CreationSalleController {
 		//int nombrelig = getRowCount(gridPane);
 		
 		for (int i = 0; i < nombreCol ; i++) {
-			gridPane.getColumnConstraints().add(new ColumnConstraints(tailleCol / (nombreCol) - 1));
+			gridPane.getColumnConstraints().add(new ColumnConstraints(tailleCol / nombreCol));
 		}
 		
 		for (int j = 0 ; j < nombreLig; j++) {
-        	gridPane.getRowConstraints().add(new RowConstraints(tailleLig / (nombreLig) - 1));
+        	gridPane.getRowConstraints().add(new RowConstraints(tailleLig / nombreLig));
         }
         pane.getChildren().add(0,gridPane);
         
@@ -53,6 +67,14 @@ public class CreationSalleController {
 				Rectangle rectangle = new Rectangle(tailleCol / (nombreCol) - 1, tailleLig / (nombreLig) - 1);
 	            rectangle.setFill(Color.GREEN);
 	            rectangle.setStroke(Color.WHITE);
+	         // evenement au click sur un rectangle
+	            rectangle.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+	            	@Override
+	            	public void handle(MouseEvent e) {
+	            	click((GridPane.getRowIndex( rectangle)+1),(GridPane.getColumnIndex( rectangle)+1), rectangle );
+	            	
+	            	}
+	            });
 	            gridPane.add(rectangle, i, j);
 	        }
 		}
@@ -78,7 +100,60 @@ public class CreationSalleController {
 		
 		initialize();
 	        	
-	        	
+	}
+	
+	@FXML
+	private void selNormal(){
+		normal.setTextFill(Color.RED);
+		handi.setTextFill(Color.BLACK);
+		porte.setTextFill(Color.BLACK);
+		couloir.setTextFill(Color.BLACK);
+	}
+	
+	@FXML
+	private void selHandi(){
+		normal.setTextFill(Color.BLACK);
+		handi.setTextFill(Color.RED);
+		porte.setTextFill(Color.BLACK);
+		couloir.setTextFill(Color.BLACK);
+	}
+	
+	@FXML
+	private void selPorte(){
+		normal.setTextFill(Color.BLACK);
+		handi.setTextFill(Color.BLACK);
+		porte.setTextFill(Color.RED);
+		couloir.setTextFill(Color.BLACK);
+	}
+	
+	@FXML
+	private void selCouloir(){
+		normal.setTextFill(Color.BLACK);
+		handi.setTextFill(Color.BLACK);
+		porte.setTextFill(Color.BLACK);
+		couloir.setTextFill(Color.RED);
+	}
+	
+	public void click(int i, int j, Rectangle rect){
+		System.out.println(i + "/" + j);
+		if (normal.getTextFill() == Color.RED)
+		{
+			rect.setFill(Color.GREEN);
+		}
+		if (handi.getTextFill() == Color.RED)
+		{
+			rect.setFill(Color.BLUE);
+		}
+		if (porte.getTextFill() == Color.RED)
+		{
+			rect.setFill(Color.BROWN);
+		}
+		if (couloir.getTextFill() == Color.RED)
+		{
+			rect.setFill(Color.BLACK);
+		}
+		
+	}
 	        	
 	            
 //
@@ -98,7 +173,7 @@ public class CreationSalleController {
 //	            gridPane.add(rectangle, i, j);
 
 		
-	}
+
 	
 //	private int getRowCount(GridPane pane) {
 //        int numRows = pane.getRowConstraints().size();
