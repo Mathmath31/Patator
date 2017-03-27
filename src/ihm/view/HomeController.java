5283aa148580fc471b8234d321fce9078f325619
mathVM31 <le_math@live.fr>
2017-03-27 20:34:24 +0200

    logo

--- src/ihm/view/HomeController.java
+++ src/ihm/view/HomeController.java
@@ -1,14 +1,30 @@
 package ihm.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 
 
-
+/**
+ * Home controller class for the entire layout.
+ * 
+ * @author MVM
+ */
 public class HomeController {
 
 	@FXML
-	private ImageView image;
+	private ImageView logo;
 	
+	/**
+	 * function called when the fxml view is called
+	 * add logo to Image view
+	 * @author MVM
+	 */
+	public void initialize(){
+		String imageURI = "";
+		imageURI = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Mars.png/220px-Mars.png";
+		Image image = new Image(imageURI);
+		logo.setImage(image);
+	}
 }
